e0241e22c7fa2d0a3862ca8a11921df306bb222d
shinjikik <shinjikik@5de396f6-0de9-11de-9c5c-7db7966a26d7>
2009-03-25 22:27:00 +0000

    Esqueleto inicial do código adicionado; Dúvidas.txt do Planejamento excluído.

---
+++ Código/TrafficAnalyzer/TrafficAnalyzer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrafficAnalyzer
+{
+	/// <summary>
+	/// TO-DO
+	/// </summary>
+	public class Analyzer
+	{
+		#region Fields
+		#endregion
+
+		#region Constructors
+		public Analyzer()
+		{
+		}
+		#endregion
+	}
+}
---
+++ Código/TrafficSimulator/TrafficSimulator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace TrafficSimulator
+{
+	/// <summary>
+	/// TO-DO
+	/// </summary>
+	public class Simulator
+	{
+		#region Fields
+		#endregion
+
+		#region Constructors
+		public Simulator()
+		{
+		}
+		#endregion
+	}
+}
